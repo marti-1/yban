7460bb6d94d8fef86c1c2c5edbf75f6153ff2293
marti-1 <martynas.miliauskas@protonmail.com>
2024-11-10 18:25:00 +0200

    dont commit compiled assets

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,21 @@ function buildStyles() {
   ])
     .pipe(sass().on('error', sass.logError))
     .pipe(concat('application.css'))
-    .pipe(gulp.dest('public/css'));
+    .pipe(gulp.dest('public'));
 }
 
+function buildScripts() {
+  return gulp.src([
+    'node_modules/jquery/dist/jquery.min.js',
+    'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+    'assets/javascripts/application.js'
+  ])
+    .pipe(concat('application.js'))
+    .pipe(gulp.dest('public'));
+}
+
+const buildAll = gulp.series(buildStyles, buildScripts);
+
 exports.buildStyles = buildStyles;
+exports.buildScripts = buildScripts;
+exports.buildAll = buildAll;
